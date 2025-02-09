3532621f9dc2f801850b21c8bd07ba277e279869
Pablo REY <61079549+PabloRey42@users.noreply.github.com>
2025-02-09 01:22:03 +0100

    update api Flask Login/register

--- Api.py
+++ Api.py
@@ -129,4 +129,4 @@ def get_rsi_for_symbol(symbol):
 
 # ========================== 🚀 LANCEMENT DU SERVEUR ==========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, ssl_context=('cert.pem', 'key.pem'))
